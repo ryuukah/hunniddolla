0d23a4688d1c2baaf15447231a3f3f25d6c1982d
ryuka <domodachiiwa@gmail.com>
2026-08-22 17:08:55 -0300

    script.js

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ const infoPage = document.getElementById("infoPage");
 const arrow = document.getElementById("arrow");
 
 aboutToggle.addEventListener("click", () => {
-    const isOpen = aboutToggle.getAttribute("aria-expanded") === "true";
+
+    const isOpen =
+        aboutToggle.getAttribute("aria-expanded") === "true";
 
     if (isOpen) {
+
         // CLOSE
         aboutToggle.setAttribute("aria-expanded", "false");
+
         infoPage.classList.remove("open");
         arrow.classList.remove("open");
+
     } else {
+
         // OPEN
         aboutToggle.setAttribute("aria-expanded", "true");
+
         infoPage.classList.add("open");
         arrow.classList.add("open");
+
     }
+
 });
